00e49a7b8316fe370a03c7b9eb9d80b5b71d7d38
smosco <go1ruf2tk3@gmail.com>
2025-01-24 16:58:40 +0900

    feat: add customer and admin selection button on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center gap-4 min-h-screen font-[family-name:var(--font-geist-sans)]'>
       <h1>Next Table Order</h1>
-      <Button>Order With Next Table Order</Button>
+      <div className='flex items-center gap-4'>
+        <Button>
+          <Link href='/customer'>Customer</Link>
+        </Button>
+        <Button>
+          <Link href='/admin'>Admin</Link>
+        </Button>
+      </div>
     </div>
   );
 }
