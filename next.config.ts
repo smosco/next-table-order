955575e411fa643cf7a8aef1a3a7abd2397d2d66
smosco <go1ruf2tk3@gmail.com>
2025-02-11 11:18:40 +0900

    feat: add option selection UI using shadcn components

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'olo-images-live.imgix.net',
       },
+      {
+        protocol: 'https',
+        hostname: 'phihwugemohmclgfnycr.supabase.co',
+      },
     ],
   },
 };
